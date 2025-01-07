43c6f5054ebfd72b69a231d283387e78b9e1f4d5
Adria <Adria@PC-ADRIA>
2025-01-07 17:48:23 +0100

    Agregar archivos de proyecto.

---
+++ Whatsapp Link Generator/Form1.vb
@@ -0,0 +1,16 @@
+﻿Imports System.Net.Mime.MediaTypeNames
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim telefono As String = tb_tel.Text
+        If Len(Trim(telefono)) > 0 Then
+            Dim enlace As String = "https://wa.me/34" & telefono
+            Process.Start("chrome.exe", enlace)
+        Else
+            MessageBox.Show("Introduce un telefono")
+        End If
+
+
+
+    End Sub
+End Class
